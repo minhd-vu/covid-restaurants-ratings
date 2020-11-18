36da99577d984ad3438ce3e9ab97c4712107af5a
Minh Vu <minhd_vu@yahoo.com>
2020-11-18 06:17:21 +0000

    pool database

--- app.js
+++ app.js
@@ -39,43 +39,53 @@ app.get("/register", (request, response) => {
 });
 
 app.post("/register", (request, response) => {
-    // Create the users table if it does not exist.
-    db.query("CREATE TABLE IF NOT EXISTS users (user VARCHAR(255) UNIQUE, password VARCHAR(255))", function (err, result) {
-        if (err) throw err;
-        console.log("Table users created.");
-    });
 
-    // Insert the username and password into the table.
-    const sql = "INSERT INTO users (user, password) VALUES ('" + request.body.user + "','" + request.body.password + "')";
-    db.query(sql, (err, result) => {
-        if (err) {
-            if (err.errno == 1062) {
-                console.log("Username already exists.");
-            } else if (err) {
-                throw err;
-            }
-        } else {
-            response.status(200).send("User registered successfully.");
-            console.log("Inserted new user into database.");
-        }
+    db.getConnection((err, connection) => {
+        if (err) throw err;
+
+        // Create the users table if it does not exist.
+        connection.query("CREATE TABLE IF NOT EXISTS users (user VARCHAR(255) UNIQUE, password VARCHAR(255))", function (err, result) {
+            if (err) throw err;
+
+            // Insert the username and password into the table.
+            const sql = "INSERT INTO users (user, password) VALUES ('" + request.body.user + "','" + request.body.password + "')";
+            connection.query(sql, (err, result) => {
+                if (err) {
+                    if (err.errno == 1062) {
+                        console.log("Username already exists.");
+                    } else if (err) {
+                        throw err;
+                    }
+                } else {
+                    response.status(200).send("User registered successfully.");
+                    console.log("Inserted new user into database.");
+                }
+            });
+        });
     });
 });
 
 app.post("/login", (request, response) => {
     const sql = "SELECT * FROM users WHERE user = '" + request.body.user + "'";
-    db.query(sql, (err, result) => {
-        if (err) throw err;
-        else if (result.length > 0) {
-            if (result[0].password === request.body.password) {
-                request.session.user = request.body.user;
 
-                response.status(200).send(request.body.user + " has logged in.");
-                console.log(request.body.user + " has logged in.");
-            } else {
-                response.status(204).send("Invalid username and password.");
-                console.log("Invalid username and password.");
+    db.getConnection((err, connection) => {
+        if (err) throw err;
+
+        connection.query(sql, (err, result) => {
+            connection.release();
+            if (err) throw err;
+            else if (result.length > 0) {
+                if (result[0].password === request.body.password) {
+                    request.session.user = request.body.user;
+
+                    response.status(200).send(request.body.user + " has logged in.");
+                    console.log(request.body.user + " has logged in.");
+                } else {
+                    response.status(204).send("Invalid username and password.");
+                    console.log("Invalid username and password.");
+                }
             }
-        }
+        });
     });
 });
 
@@ -85,22 +95,27 @@ app.get("/search", (request, response) => {
 
 app.post("/search", (request, response) => {
     const sql = "SELECT * FROM places WHERE place_id = '" + request.body.place_id + "'";
-    db.query(sql, (err, result) => {
+    db.getConnection((err, connection) => {
         if (err) throw err;
-        else if (result.length > 0) {
-            let json = '{ "reviews" : [';
-            for (let i = 0; i < result.length; ++i) {
-                json += '{ "user":"' + result[i].user + '" , "rating":' + result[i].rating + ' , "comment":"' + result[i].comment + '" }'
-                if (i != result.length - 1) {
-                    json += ",";
-                }
-            }
-            json += ' ]}';
 
-            response.status(200).json(JSON.parse(json));
-        } else {
-            response.status(204).send("No covid reviews.");
-        }
+        connection.query(sql, (err, result) => {
+            connection.release();
+            if (err) throw err;
+            else if (result.length > 0) {
+                let json = '{ "reviews" : [';
+                for (let i = 0; i < result.length; ++i) {
+                    json += '{ "user":"' + result[i].user + '" , "rating":' + result[i].rating + ' , "comment":"' + result[i].comment + '" }'
+                    if (i != result.length - 1) {
+                        json += ",";
+                    }
+                }
+                json += ' ]}';
+
+                response.status(200).json(JSON.parse(json));
+            } else {
+                response.status(204).send();
+            }
+        });
     });
 });
 
@@ -115,25 +130,25 @@ app.get("/review", (request, response) => {
 
 app.post("/review", (request, response) => {
     if (request.session.user) {
-        // Create the places table if it does not exist.
-        db.query("CREATE TABLE IF NOT EXISTS places (place_id VARCHAR(255), user VARCHAR(255), rating INT, comment TEXT)", (err, result) => {
-            if (err) {
-                throw err;
-            }
-        });
+        db.getConnection((err, connection) => {
+            if (err) throw err;
 
-        // Insert the review into the table.
-        const sql = "INSERT INTO places (place_id, user, rating, comment) VALUES ('" + request.body.place_id + "','" + request.session.user + "','" + request.body.rating + "','" + request.body.comment + "')";
-        db.query(sql, (err, result) => {
-            if (err) {
-                throw err;
-            } else {
-                response.status(200).send("Review entered succesfully.");
-                console.log("Added review by " + request.session.user + " for place " + request.body.place_id + " into database.");
-            }
+            // Create the places table if it does not exist.
+            connection.query("CREATE TABLE IF NOT EXISTS places (place_id VARCHAR(255), user VARCHAR(255), rating INT, comment TEXT)", (err, result) => {
+                if (err) throw err;
+
+                // Insert the review into the table.
+                const sql = "INSERT INTO places (place_id, user, rating, comment) VALUES ('" + request.body.place_id + "','" + request.session.user + "','" + request.body.rating + "','" + request.body.comment + "')";
+                connection.query(sql, (err, result) => {
+                    connection.release();
+                    if (err) throw err;
+                    response.status(200).send("Review entered succesfully.");
+                    console.log("Added review by " + request.session.user + " for place " + request.body.place_id + " into database.");
+                });
+            });
         });
     } else {
-        response.status(204).send("Must be logged in to submit a review.");
+        response.status(230).send("Must be logged in to submit a review.");
     }
 });
 
